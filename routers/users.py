051278890d8244f64e22184f2045f483eb65aea6
Jonatas E S Silva <jonatassilvaemanuel@gmail.com>
2023-08-18 02:12:26 -0300

    fix: application ran now, our tests broke

--- routers/users.py
+++ routers/users.py
@@ -36,7 +36,7 @@ def create_user(user: UserSchema, session: Session):
 
 
 @router.get('/', response_model=UserList)
-def read_users(skip: int = 0, limit: int = 100, session: Session):
+def read_users(session: Session, skip: int = 0, limit: int = 100):
     users = session.scalars(select(User).offset(skip).limit(limit)).all()
     return {'users': users}
 
